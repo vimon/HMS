d43b184a43ba95d9e425ddaefc69613a48f20b1f
vimon <vimonpv@gmail.com>
2018-10-16 13:55:07 +0530

    updated with simple codes

--- login.php
+++ login.php
@@ -2,29 +2,28 @@
    include("config.php");
    session_start();
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+   
       
-      $myusername = mysqli_real_escape_string($conn,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+      $username =$_POST['username'];
+      $password =$_POST['password']; 
       
-      $sql = "SELECT id FROM users WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT * FROM users WHERE username = '$username' and passcode = '$password'";
       $result = mysqli_query($conn,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+      $row = mysqli_fetch_array($result);
       
+
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
             echo "$count";
          
       if($count == 1) {
-            $_SESSION['login_user'] = $myusername;
+            $_SESSION['login_user'] = $username;
          header("location: welcome.php");
       }else {
          $error = "Your Login Name or Password is invalid";
       }
-   }
+
 ?>
 <html>
    
