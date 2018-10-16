82877d3af9d82c2e196b3491d6b2dabd50a0c813
vimon <vimonpv@gmail.com>
2018-10-16 15:01:01 +0530

    Session problem solved

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
       
       $sql = "SELECT * FROM users WHERE username = '$username' and passcode = '$password'";
       $result = mysqli_query($conn,$sql);
-      $row = mysqli_fetch_array($result);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       
 
       $count = mysqli_num_rows($result);
