3b6b23509ae0c308b1d4820c5d77dc0b4faa4ea1
Vimon <vimonpv@gmail.com>
2018-10-16 14:38:54 +0530

    Update session.php

--- session.php
+++ session.php
@@ -6,7 +6,7 @@
    
    $ses_sql = mysqli_query($conn,"select username from users where username = '$user_check' ");
    
-   $row = mysqli_fetch_array($ses_sql,);
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
    $login_session = $row['username'];
    
